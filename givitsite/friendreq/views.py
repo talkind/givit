40af76980e707554179edc7a136e5c6367100030
nadav shoshan <nadavshoshan@gmail.com>
2020-08-27 12:33:00 +0300

    update friend requeat and add postgresql support
    small url chages ti friendrequest
    
    delete db.sqlite3 file
    
    we chaghed our db to postgresql
    
    update the friendreq app
    
    1. chnged the itemrequest table the the agreed format
    
    2. add one more featuere that allows a friend to aske for several items
       (not just one)
    
    update the html pages (templates)
    
    1.since we are looking for specific keywords on the relevant website we
      prefer to limit the friends to choose from a list (city and item)/
      therefore, i add ascroll down input in the html form.
    
    2. every friend should be able to request many items, so. I added an key
       which return him to the request form aftyer he submited a request
    
    new line charcter EOF
    
    fixing typo

--- givitsite/friendreq/views.py
+++ givitsite/friendreq/views.py
@@ -12,7 +12,7 @@ def requestItem(request):
         city_val = request.POST["city"]
         item_val = request.POST["item"]
         special_val = request.POST["specialreq"]
-        newReq = ItemRequest(item=item_val,city = city_val,special_req = special_val)
+        newReq = ItemRequest(item=item_val,city = city_val,special_req = special_val,friend_id = 305355356)
         newReq.save();
         print('new item added')
         return render(request, 'demandDB.html')
