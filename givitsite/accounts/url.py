b7db402a8c4a9f3f0a357a8d1ea2eca6b41d4f5e
talkind <talkind24@gmail.com>
2020-08-31 09:25:03 +0000

    set up user management app we want to be able to show different pages to friend and coordinators we want to be able to register new users we want to be able to login and logout as a registered users we want to be able to add and delete by admin i build an app to login i build an app to register a new user (only a friend) i create a new table for user managment db i allowed to communicate with the db by html page i nodefied home page to present relevant options

--- givitsite/accounts/url.py
+++
@@ -1,6 +0,0 @@
-from django import path
-from . import views
-
-urlpatterns = [
-    path("register", views.register, name = "register")
-]
--- givitsite/accounts/views.py
+++ givitsite/accounts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User, auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
+# from .models import Users
+
 # Create your views here.
 
 def register(request):
@@ -7,15 +10,58 @@ def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        password1 = request.POST['password1']
-
+        username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+        date_of_birth = request.POST['date_of_birth']
+        address_street = request.POST['address_street']
+        address_home_number = request.POST['address_home_number']
+        address_city = request.POST['address_city']
+        phone_number = request.POST['phone_number']
+        field_of_study = request.POST['field_of_study']
+        degree = request.POST['degree']
+        current_year = request.POST['current_year']
+        scholarship = request.POST['scholarship']
+        type_of_friendship = request.POST['type_of_friendship']
+        email = request.POST['email']
 
-        user = User.objects.create_user(username = username, password = password1, email = email, first_name = first_name, last_name = last_name)
-        user.save();
-
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username = username, email = email, password = password1, first_name = first_name, last_name = last_name, address_street = address_street, date_of_birth = date_of_birth, address_home_number = address_home_number, address_city = address_city, phone_number = phone_number, field_of_study = field_of_study, degree = degree, current_year = current_year, scholarship = scholarship, type_of_friendship = type_of_friendship)
+                user.save();
+                return redirect('home.html')
+        else:
+            messages.info(request,'password not match')
+            return redirect('register')
         return redirect('/')
 
     else:
         return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
