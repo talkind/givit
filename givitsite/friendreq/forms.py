3f419daac5b95eb8f214e6df14ce8dfca65eb7f3
liorsicd <lior.cd@gmail.com>
2020-09-02 11:08:27 +0300

    create model-form
    we learned that it's better to user django model form to generate HTML
    form from a model (and not build the form manually)
    this form object is sent to the HTML file by the view.

---
+++ givitsite/friendreq/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from . models import ItemRequest
+
+class itemRequestForm(forms.ModelForm):
+    class Meta:
+        model = ItemRequest
+        fields = [
+            'item',
+            'region',
+            'special_req'
+        ]
