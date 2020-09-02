5eaf2f5e04c106ab36762ed7be5cf1f84dd01ac6
talkind <talkind24@gmail.com>
2020-09-03 00:34:51 +0300

    small changes to support accounts app

--- givitsite/givitsite/urls.py
+++ givitsite/givitsite/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('home.urls')),
     path('friend/', include('friendreq.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
