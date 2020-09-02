2bb8f3985bd30e4bd4f04a8fddc233cacb5351e6
talkind <talkind24@gmail.com>
2020-09-03 00:34:51 +0300

    small changes to support accounts app

--- givitsite/givitsite/settings.py
+++ givitsite/givitsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -31,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+    'crispy_forms',
+    'givitsite',
     'friendreq',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
