efc062571877743c529a6d1d69cc65632ddae1c5
liorsicd <lior.cd@gmail.com>
2020-09-02 11:00:14 +0300

    edit app models
    1. create new model for friendfeed feature
    2. change 'request' model - using enums (close lists of options)

--- givitsite/friendreq/models.py
+++ givitsite/friendreq/models.py
@@ -1,11 +1,47 @@
 from django.db import models
 
 # Create your models here.
+REGION_CHOICES = [
+    ('Tel Aviv','Tel Aviv'),
+    ('Jerusalem','Jerusalem'),
+    ('Beer Sheva','Beer Sheva'),
+    ('Haifa','Haifa'),
+    ('Modiin','Modiin'),
+    ('Hasharon','Hasharon'),
+]
+
+ITEM_CHOICES = [
+    ('table','table'),
+    ('bed','bed'),
+    ('TV','TV'),
+    ('washing machine','washing machine'),
+    ('fridge','fridge'),
+    ('oven','oven'),
+    ('microwave oven','microwave oven'),
+    ('chair','chair'),
+    ('computer screen','computer screen'),
+    ('keyboard','keyboard'),
+    ('laptop','laptop'),
+    ('closet','closet'), 
+]
+STATUS_CHOICES = [
+    ('open','open'),
+    ('closed','closed'),
+]
 
 class ItemRequest(models.Model):
-    friend_id = models.IntegerField()
-    item = models.CharField(max_length = 40)
-    city = models.CharField(max_length = 40)
+    #friennd ID inherite from user table
+    friend_id = models.IntegerField(default = 305355356)
+    item = models.CharField(max_length = 40,choices = ITEM_CHOICES)
+    region = models.CharField(max_length = 40,choices = REGION_CHOICES)
     special_req= models.TextField()
-    isOpen = models.BooleanField(default = True)
+    status = models.CharField(max_length = 40,default = 'open', choices = STATUS_CHOICES)
     
+    
+class ItemsFound(models.Model):
+    request_id = models.IntegerField()
+    url = models.CharField(max_length=200)
+    picture = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=12) 
