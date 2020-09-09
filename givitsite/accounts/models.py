bf48b887713a1ad669ef9f4ee64981cd33ad919a
talkind <talkind24@gmail.com>
2020-09-09 23:10:18 +0300

    add a new model for personal user data
    added a new model with all extra data field needed in registration

--- givitsite/accounts/models.py
+++ givitsite/accounts/models.py
@@ -1,5 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEMBERSHIP_TYPE = [
+    ('friend','friend'),
+    ('coordinator','coordinator'),
+]
 
+DEGREE = [
+    ('BA', 'BA'),
+    ('BSC', 'BSC'),
+    ('MA', 'MA'),
+    ('MSC', 'MSC'),
+    ('PHD', 'PHD'),
+]
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    confirmed = models.BooleanField("confirmed", default=False)
+    student_id = models.IntegerField()
+    phone_number = models.IntegerField()
+    date_of_birth = models.DateField()
+    home_address = models.CharField(max_length=100)
+    field_of_study = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100, default='BA', choices = DEGREE)
+    scholarship = models.CharField(max_length=100)
+    membership_type = models.CharField(max_length=100, default= 'friend', choices=MEMBERSHIP_TYPE)
+
+    def _str_(self):
+        return self.user.username
--- givitsite/accounts/views.py
+++ givitsite/accounts/views.py
@@ -2,42 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import auth, User
 from django.contrib import messages
-from accounts.forms import RegistrationForm
-
-# Create your views here.
+from .forms import RegistrationForm, PersonInfoForm
 
 def register(request):
-
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-    
-    else:
-        form = RegistrationForm()
-        args = {'form': form}
+        user_form = RegistrationForm(request.POST)
+        profile_form = PersonInfoForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+
+            return redirect('/')
+    else:        
+        user_form = RegistrationForm()
+        profile_form = PersonInfoForm()
+    args = {'user_form': user_form, 'profile_form': profile_form}
     return render(request, 'register.html', args)
-
-    #     if password1 == password2:
-    #         if User.objects.filter(username=username).exists():
-    #             messages.info(request,'Username Taken')
-    #             return redirect('register')
-    #         elif User.objects.filter(email=email).exists():
-    #             messages.info(request,'email Taken')
-    #             return redirect('register')
-    #         else:
-    #             user = User.objects.create_user(username = username, email = email, password = password1, first_name = first_name, last_name = last_name, address_street = address_street, date_of_birth = date_of_birth, address_home_number = address_home_number, address_city = address_city, phone_number = phone_number, field_of_study = field_of_study, degree = degree, current_year = current_year, scholarship = scholarship, type_of_friendship = type_of_friendship)
-    #             user.save();
-    #             return redirect('home.html')
-    #     else:
-    #         messages.info(request,'password not match')
-    #         return redirect('register')
-    #     return redirect('/')
-
-    # else:
-    #     return render(request, 'register.html')
-
 
 def login(request):
     if request.method == 'POST':
