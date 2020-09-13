565bb9f41b9e3f484db1f2df1435345686a5f15d
Matan2455 <Matan.sinai@sap.com>
2020-09-13 22:50:27 +0300

    modify sys.path in _init_ file

---
+++ givitsite/coordinate/__init__.py
@@ -0,0 +1,4 @@
+import sys
+
+#add parent directory to sys.path in order to allow imports from sibling modules
+sys.path.append('../')
--- givitsite/coordinate/admin.py
+++ givitsite/coordinate/admin.py
@@ -1,6 +1,5 @@
-import sys
 from django.contrib import admin
 from . models import CoordinatedItems
 
-sys.path.append('../')
+
 admin.site.register(CoordinatedItems)
