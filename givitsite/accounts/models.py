9bd7eae53c52a111081c4e23382439a398f2ee91
talkind <talkind24@gmail.com>
2020-09-28 19:00:07 +0300

    small changes in profile model
    changed the regex expressions
    remove 'help_text' from the fields

--- givitsite/accounts/models.py
+++ givitsite/accounts/models.py
@@ -23,7 +23,8 @@ FIELD_OF_STUDY = [
     ('Economics', 'Economics'),
     ('Law', 'Law'),
     ('Psychology', 'Communications'),
-    ('Government, Diplomacy and Strategy', 'Government, Diplomacy and Strategy'),
+    ('Government, Diplomacy and Strategy',
+     'Government, Diplomacy and Strategy'),
 ]
 
 GENDER = [
@@ -36,24 +37,25 @@ GENDER = [
 class Profile(models.Model):
 
     phone_regex = RegexValidator(
-        regex=r'^05[0-68-9][0-9]{7}$', message="Phone number must be entered in the format: 05XXXXXXXX.")
+        regex=r'^05[0-68-9][0-9]{7}$',
+        message="Phone number must be entered in the format: 05XXXXXXXX.")
     id_regex = RegexValidator(
-        regex=r'[0-9]{8,9}', message="ID must be 9 digits long.")
+        regex=r'\d{8,9}', message="ID must be 9 digits long.")
     house_number_regex = RegexValidator(
-        regex=r'[0-9]{1-3}', message="Please enter a valid number from 1 to 999.")
+        regex=r'\d{1,3}', message="Please enter a valid number from 1 to 999.")
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
 
     confirmed = models.BooleanField("confirmed", default=False)
     student_id = models.CharField(max_length=10, validators=[id_regex])
     gender = models.CharField(max_length=10, default='other', choices=GENDER)
-    phone_number = models.CharField(
-        help_text='example: 0544544554', max_length=10, validators=[phone_regex])
-    date_of_birth = models.DateField(max_length=8, help_text='mm/dd/yyyy')
+    phone_number = models.CharField(max_length=10, validators=[phone_regex])
+    date_of_birth = models.DateField(max_length=8)
     address_street_name = models.CharField(
         max_length=50, verbose_name='street')
     address_house_number = models.CharField(
-        max_length=3, verbose_name='home_number', validators=[house_number_regex])
+        max_length=3, verbose_name='home_number',
+        validators=[house_number_regex])
     address_city = models.CharField(max_length=50, verbose_name='city')
     field_of_study = models.CharField(
         max_length=100, default='Computer Science', choices=FIELD_OF_STUDY)
