5a89d9aae29e8c3d7f418dfd5c4eefc538ac044f
talkind <talkind24@gmail.com>
2020-09-28 11:46:14 +0300

    small modification in profile model
    small changes in regex expressions
    remove 'help_text'

--- givitsite/accounts/models.py
+++ givitsite/accounts/models.py
@@ -35,21 +35,17 @@ GENDER = [
 
 class Profile(models.Model):
 
-    phone_regex = RegexValidator(
-        regex=r'^05[0-68-9][0-9]{7}$', message="Phone number must be entered in the format: 05XXXXXXXX.")
-    id_regex = RegexValidator(
-        regex=r'[0-9]{8,9}', message="ID must be 9 digits long.")
-    house_number_regex = RegexValidator(
-        regex=r'[0-9]{1-3}', message="Please enter a valid number from 1 to 999.")
+    phone_regex = RegexValidator(regex=r'^05[0-68-9][0-9]{7}$', message="Phone number must be entered in the format: 05XXXXXXXX.")
+    id_regex = RegexValidator(regex=r'\d{8,9}', message="ID must be 9 digits long.")
+    house_number_regex = RegexValidator(regex=r'\d{1,3}', message="Please enter a valid number from 1 to 999.")
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
 
     confirmed = models.BooleanField("confirmed", default=False)
     student_id = models.CharField(max_length=10, validators=[id_regex])
     gender = models.CharField(max_length=10, default='other', choices=GENDER)
-    phone_number = models.CharField(
-        help_text='example: 0544544554', max_length=10, validators=[phone_regex])
-    date_of_birth = models.DateField(max_length=8, help_text='mm/dd/yyyy')
+    phone_number = models.CharField(max_length=10, validators=[phone_regex])
+    date_of_birth = models.DateField(max_length=8)
     address_street_name = models.CharField(
         max_length=50, verbose_name='street')
     address_house_number = models.CharField(
@@ -59,8 +55,7 @@ class Profile(models.Model):
         max_length=100, default='Computer Science', choices=FIELD_OF_STUDY)
     degree = models.CharField(max_length=100, default='BA', choices=DEGREE)
     scholarship = models.CharField(max_length=100)
-    membership_type = models.CharField(
-        max_length=100, default='friend', choices=MEMBERSHIP_TYPE)
+    membership_type = models.CharField(max_length=100, default='friend', choices=MEMBERSHIP_TYPE)
 
     def _str_(self):
         return self.user.username
